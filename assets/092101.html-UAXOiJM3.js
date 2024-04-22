a1011256225895f977930be73cc261182efb8af7
schawcheng <schawcheng@gmail.com>
2024-04-23 00:19:57 +0800

    deploy

---
+++ assets/092101.html-UAXOiJM3.js
@@ -0,0 +1,4 @@
+const data = JSON.parse('{"key":"v-2eb9b31a","path":"/blogs/category1/2019/092101.html","title":"second page in category1","lang":"en-US","frontmatter":{"title":"second page in category1","date":"2019/09/21","tags":["tag2"],"categories":["category1"]},"headers":[],"git":{},"filePathRelative":"blogs/category1/2019/092101.md"}');
+export {
+  data
+};
---
+++ assets/index.html-m8yp8bxa.js
@@ -0,0 +1,4 @@
+const data = JSON.parse(`{"key":"v-8daa1a0e","path":"/","title":"","lang":"en-US","frontmatter":{"home":true,"modules":["BannerBrand","Blog","MdContent","Footer"],"bannerBrand":{"bgImage":"/bg.svg","title":"Schawcheng's blog","description":"万丈高楼平地起，勿在浮沙筑高台","tagline":"我将在此分享我工作与学习中遇到的枯燥但有用和有意思的知识，以及技术问题的解决方法，主要与数学、计算机相关。"},"blog":{"socialLinks":[{"icon":"LogoGithub","link":"https://github.com/recoluan"}]},"isShowTitleInHome":false,"actionText":"About","actionLink":"/views/other/about"},"headers":[],"git":{},"filePathRelative":"README.md"}`);
+export {
+  data
+};
